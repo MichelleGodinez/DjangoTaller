2b95b162d502afddab2c17f82fb420d3a2cb67b7
MichelleGodinez <miche.rommz@gmail.com>
2016-11-10 13:58:40 +0100

    Agregando aun mas archivos

--- alumnos/datos/models.py
+++ alumnos/datos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class InfoGeneral(models.Model):
+	nombre=models.CharField(max_length=66, default="Juanito")
+	apellido = models.CharField(max_length=50, default="")
+	telefono = models.CharField(max_length=50)
+	fecha = models.DateField()
+	semestre = models.CharField(max_length=50)
+	carrera = models.CharField(max_length=50)
+	SEXO_OPTIONS = (
+		('F', 'FEMENINO'),
+		('M', 'MASCULINO'),
+		)
+	sexo = models.CharField(max_length=50, choices=SEXO_OPTIONS)
+	direccion = models.CharField(max_length=100)
+	estructura = models.CharField(max_length=50, default="")
